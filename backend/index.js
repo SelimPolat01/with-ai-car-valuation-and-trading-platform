d907ec39abea5209f06d4cc5e43e5e229377516f
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-06-14 22:53:16 +0300

    render integrated and deleted socket.io

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,13 @@ import { router as advertsRoutes } from "./routes/adverts.js";
 import { router as predictRoutes } from "./routes/predict.js";
 import { router as personalInfoRoutes } from "./routes/infos.js";
 import http from "http";
-import { Server } from "socket.io";
 import { db } from "./lib/db.js";
 import { rateLimit } from "express-rate-limit";
 import path from "path";
-
 // import { dbInsertCars } from "./utils/dbInsertCars.js";
 
 const app = express();
+
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -25,60 +24,15 @@ const globalLimiter = rateLimit({
   standardHeaders: true,
   legacyHeaders: false,
 });
-const PORT = Number(process.env.PORT);
+
+const PORT = Number(process.env.PORT) || 3000;
+const FRONTEND_URL = process.env.FRONT_END_URL || "*";
+
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-const onlineUsers = new Map();
-
-io.on("connection", (socket) => {
-  console.log("Bir kullanıcı bağlandı: ", socket.id);
-
-  socket.on("register", (userId) => {
-    console.log(`Kullanıcı ${userId} kaydoldu.`);
-    onlineUsers.set(userId.toString(), socket.id);
-  });
-
-  socket.on(
-    "sendMessageToUser",
-    async ({ senderId, receiverId, advertId, message }) => {
-      console.log(`Mesaj gönderiliyor: ${message} -> ${receiverId}`);
-      try {
-        const result = await db.query(
-          "INSERT INTO messages (user_id, receiver_id, advert_id, message) VALUES ($1, $2, $3, $4) RETURNING *",
-          [senderId, receiverId, advertId, message],
-        );
-        const savedMessage = result.rows[0];
-        const receiverSocketId = onlineUsers.get(receiverId.toString());
-        const senderSocketId = onlineUsers.get(senderId.toString());
-
-        if (receiverSocketId)
-          io.to(receiverSocketId).emit("receiveMessage", savedMessage);
-
-        if (senderSocketId)
-          io.to(senderSocketId).emit("receiveMessage", savedMessage);
-      } catch (err) {
-        console.log(err);
-      }
-    },
-  );
-
-  socket.on("disconnect", () => {
-    console.log("Bir kullanıcı ayrıldı: ", socket.id);
-    for (let [userId, socketId] of onlineUsers.entries()) {
-      if (socketId === socket.id) onlineUsers.delete(userId);
-    }
-  });
-});
 
 app.use(
   cors({
-    origin: process.env.FRONT_END_URL,
+    origin: FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -94,6 +48,6 @@ app.use("/adverts", globalLimiter, advertsRoutes);
 app.use("/predict", globalLimiter, predictRoutes);
 app.use("/infos", personalInfoRoutes);
 
-server.listen(PORT, () => {
-  console.log(`Server ve Socket.IO ${PORT} portunda çalışıyor.`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server ${PORT} portunda başarıyla çalışıyor.`);
 });

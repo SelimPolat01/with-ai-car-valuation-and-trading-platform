9072ad7ec4e6069cd0e14e3032e33a67ddfce913
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-06-15 20:03:27 +0300

    fixed.

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -16,30 +16,51 @@ db.query("SELECT NOW()")
 
 export async function createTable() {
   const query = `
-    ALTER TABLE cars
-ADD CONSTRAINT unique_car_features UNIQUE (
-    brand, 
-    model, 
-    model_year, 
-    body_type, 
-    engine_capacity, 
-    horsepower, 
-    transmission, 
-    kilometer, 
-    fuel_type, 
-    price, 
-    trim_level
-);
-
-   CREATE TABLE advert_images (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+   CREATE TABLE favorite_adverts (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
     advert_id INT NOT NULL,
-    image_url TEXT NOT NULL,
-    is_main BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id) 
+        REFERENCES users (id)
+        ON DELETE CASCADE,
     CONSTRAINT fk_advert
         FOREIGN KEY (advert_id) 
-        REFERENCES advert (id)
-        ON DELETE CASCADE
+        REFERENCES adverts (id)
+        ON DELETE CASCADE,
+    -- Aynı kullanıcının aynı ilanı birden fazla kez favorilere eklemesini önlemek için:
+    CONSTRAINT unique_user_favorite UNIQUE (user_id, advert_id)
+);
+
+   CREATE TABLE sold_adverts (
+    id SERIAL PRIMARY KEY,
+    brand VARCHAR(100) NOT NULL,
+    model VARCHAR(150) NOT NULL,
+    model_year INTEGER NOT NULL,
+    body_type VARCHAR(50) NOT NULL,
+    engine_capacity INTEGER NOT NULL,
+    horsepower INTEGER NOT NULL,
+    transmission VARCHAR(50) NOT NULL,
+    kilometer INTEGER NOT NULL,
+    fuel_type VARCHAR(50) NOT NULL,
+    price BIGINT NOT NULL,
+    trim_level VARCHAR(150) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    sold_at TIMESTAMP NOT NULL,
+    days_to_sell INTEGER NOT NULL,
+    has_scratch BOOLEAN NOT NULL,
+    has_dent BOOLEAN NOT NULL,
+    user_id INT NOT NULL,
+    advert_id INT NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id) 
+        REFERENCES users (id) 
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_advert
+        FOREIGN KEY (advert_id) 
+        REFERENCES adverts (id) 
+        ON DELETE RESTRICT
 );
   `;
 

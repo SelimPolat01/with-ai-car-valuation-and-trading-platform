676d4611411dfed1cc8523fb77df460626ff41ff
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-06-15 20:48:11 +0300

    fixed.

--- backend/routes/predict.js
+++ backend/routes/predict.js
@@ -6,15 +6,24 @@ export const router = express.Router();
 router.post("/", verifyToken, async (req, res) => {
   const carData = req.body;
   try {
-    const response = await fetch(`${process.env.FAST_API_URL}/predict`, {
+    const fetchUrl = `${process.env.FAST_API_URL}/predict`;
+    console.log("👉 NODE.JS'IN ISTEK ATTIGI TAM ADRES:", fetchUrl);
+
+    const response = await fetch(fetchUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(carData),
     });
-    if (!response.ok)
-      throw new Error("FastAPI modelinden fiyat teklifi alınamadı.");
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("🔴 GİDİLEN ADRESTEN DÖNEN HATA KODU:", response.status);
+      console.error("🔴 DÖNEN GERÇEK HATA MESAJI:", errorText);
+      throw new Error(`FastAPI'ye ulaşılamadı. Kod: ${response.status}`);
+    }
+
     const data = await response.json();
     return res.status(200).json({ price: data.predicted_price });
   } catch (err) {

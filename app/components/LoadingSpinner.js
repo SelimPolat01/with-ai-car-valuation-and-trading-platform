caa6c8e4c7ab9a4eb81bb20f8a9cdf21a03c8da4
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-06-16 19:22:45 +0300

    loading spinner enchanced.

--- app/components/LoadingSpinner.js
+++ app/components/LoadingSpinner.js
@@ -1,9 +1,9 @@
+import classes from "./LoadingSpinner.module.css";
+
 export default function LoadingSpinner() {
   return (
-    <div className="loadingWrapper">
-      <div className="loading">
-        <div className="spinner"></div>
-      </div>
+    <div className={classes.loadingWrapper}>
+      <div className={classes.loading}></div>
     </div>
   );
 }

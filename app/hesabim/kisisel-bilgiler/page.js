f6414bda25e9384dfa6ad16ce3cc9a462b9f605f
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-06-16 03:59:37 +0300

    fixed.

--- app/hesabim/kisisel-bilgiler/page.js
+++ app/hesabim/kisisel-bilgiler/page.js
@@ -17,9 +17,9 @@ export default function Hesabim() {
   const [isSuccess, setIsSuccess] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const DEFAULT_ROBOT_AVATAR =
-    "https://api.dicebear.com/8.x/bottts/svg?seed=Anonymous&baseColor=adb5bd";
-  const [imagePreview, setImagePreview] = useState(DEFAULT_ROBOT_AVATAR);
+  const DEFAULT_BLANK_AVATAR =
+    "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22%23e0e0e0%22%2F%3E%3Cpath%20d%3D%22M50%2050c11.046%200%2020-8.954%2020-20s-8.954-20-20-20-20%208.954-20%2020%208.954%2020%2020%2020zm0%2010c-15.012%200-45%207.525-45%2022.5V100h90V82.5C95%2067.525%2065.012%2060%2050%2060z%22%20fill%3D%22%239e9e9e%22%2F%3E%3C%2Fsvg%3E";
+  const [imagePreview, setImagePreview] = useState(DEFAULT_BLANK_AVATAR);
 
   useEffect(() => {
     const currentToken = localStorage.getItem("token");

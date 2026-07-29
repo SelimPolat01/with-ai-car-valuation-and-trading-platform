19615f59590cc5deba95f09207b79a7e4e85a021
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-07-29 03:03:58 +0300

    added the tanstack Query custom hook for frequently asked questions.

---
+++ hooks/GET/useGetFaqs.js
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { Fetch } from "@/backend/lib/fetch";
+
+export async function getFaqs() {
+  return await Fetch(null, "infos", "faqs", "GET", null);
+}
+
+export default function useGetFaqs() {
+  return useQuery({
+    queryFn: () => getFaqs(),
+    queryKey: ["faqs"],
+    retry: false,
+  });
+}

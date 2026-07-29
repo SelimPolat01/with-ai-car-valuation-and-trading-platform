42b2a35ec2705195a3f3db7fd421e2f387c73d84
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-07-29 03:20:17 +0300

    frequently asked questions have been removed from the 'infos' backend route.

--- backend/routes/infos.js
+++ backend/routes/infos.js
@@ -222,26 +222,3 @@ router.delete("/account", verifyToken, async (req, res) => {
     res.status(500).json({ message: "Silme işlemi hatası: " + err.message });
   }
 });
-
-router.get("/faqs", async (req, res) => {
-  const queryText = "SELECT * FROM faqs ORDER BY id ASC";
-
-  try {
-    const result = await db.query(queryText);
-
-    if (result.rows.length === 0) {
-      return res.status(404).json({
-        message: "Henüz kayıtlı Sıkça Sorulan Soru bulunamadı.",
-      });
-    }
-
-    return res.status(200).json(result.rows);
-  } catch (err) {
-    console.error("Sıkça Sorulan Sorular hatası:", err?.message);
-
-    return res.status(500).json({
-      message:
-        "Sıkça Sorulan Sorular getirilirken bir sunucu hatası meydana geldi.",
-    });
-  }
-});

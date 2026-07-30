acdcd69013b82094a344d9313b953df2332be9e1
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-07-31 00:08:09 +0300

    custom tanstack query hooks have been upgraded.

--- hooks/POST/usePostEmailVerify.js
+++ hooks/POST/usePostEmailVerify.js
@@ -1,12 +1,13 @@
 import { Fetch } from "@/backend/lib/fetch";
 import { useMutation } from "@tanstack/react-query";
 
-async function postEmailVerify(body) {
-  return await Fetch(null, "api", "email", "POST", body);
+async function postEmailVerify(body, forLogin) {
+  const queryString = forLogin ? "forLogin=true" : "forLogin=false";
+  return await Fetch(null, "api", `email?${queryString}`, "POST", body);
 }
 
 export function usePostEmailVerify() {
   return useMutation({
-    mutationFn: ({ body }) => postEmailVerify(body),
+    mutationFn: ({ body, forLogin }) => postEmailVerify(body, forLogin),
   });
 }
--- hooks/POST/usePostOtpVerify.js
+++ hooks/POST/usePostOtpVerify.js
@@ -1,12 +1,13 @@
 import { Fetch } from "@/backend/lib/fetch";
 import { useMutation } from "@tanstack/react-query";
 
-async function postOtpVerify(body) {
-  return await Fetch(null, "api", "otp", "POST", body);
+async function postOtpVerify(body, forLogin) {
+  const queryString = forLogin ? "forLogin=true" : "forLogin=false";
+  return await Fetch(null, "api", `otp?${queryString}`, "POST", body);
 }
 
 export function usePostOtpVerify() {
   return useMutation({
-    mutationFn: ({ body }) => postOtpVerify(body),
+    mutationFn: ({ body, forLogin }) => postOtpVerify(body, forLogin),
   });
 }

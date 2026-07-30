a7db229161efe20f1fd95e55a8908119cffa41cc
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-07-31 00:10:24 +0300

    a custom tanstack query hook for registration has been added.

---
+++ hooks/POST/usePostRegister.js
@@ -0,0 +1,12 @@
+import { Fetch } from "@/backend/lib/fetch";
+import { useMutation } from "@tanstack/react-query";
+
+async function postRegister(body) {
+  return await Fetch(null, "api", "register", "POST", body);
+}
+
+export function usePostRegister() {
+  return useMutation({
+    mutationFn: ({ body }) => postRegister(body),
+  });
+}

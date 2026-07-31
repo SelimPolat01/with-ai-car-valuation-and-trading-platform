16c13fa3a7a4b8a214f7dafb13fbe573157aecb9
Selim Polat <selim.polat1@ogr.sakarya.edu.tr>
2026-08-01 02:58:24 +0300

    a new tanstack query hook has been added.

---
+++ hooks/GET/useGetCheckFavoriteAdvert.js
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { Fetch } from "@/backend/lib/fetch";
+
+export async function getCheckFavoriteAdvert(advertId) {
+  return await Fetch("adverts", `check-favorite/${advertId}`, "GET", null);
+}
+
+export function useGetCheckFavoriteAdvert(advertId) {
+  return useQuery({
+    queryKey: ["checkFavorite", advertId],
+    queryFn: () => getCheckFavoriteAdvert(advertId),
+    enabled: !!advertId,
+    retry: false,
+  });
+}
